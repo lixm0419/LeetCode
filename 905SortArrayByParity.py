d1ebb4657d7e1df2a1cc84131541923a6f583e55
Xiaomeng Li <xiaomengli@Xiaomengs-MacBook-Pro.local>
2018-10-21 11:08:16 -0400

    example

---
+++ 905SortArrayByParity.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortArrayByParity(self, A):
+        start, end = 0, len(A) - 1 
+        while start <end:
+            m, n = A[start], A[end]
+            if m % 2 == 1 and n % 2 == 0
+ 
