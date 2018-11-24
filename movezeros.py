8113d6adec99717659687f9ef8c43a08a5e55266
Xiaomeng Li <xiaomengli@Xiaomengs-MacBook-Pro.local>
2018-11-24 01:05:24 -0500

    find disappear numbers

--- movezeros.py
+++ movezeros.py
@@ -7,16 +7,18 @@ Created on Thu Nov 22 23:41:52 2018
 """
 
 
-count=nums.count(0)
-while count>=0:
-    nums.remove(0)
-    nums.append(0)
-    count-=1
+def findDisappearedNumbers(nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            index = abs(nums[i])-1
+            nums[index]=-abs(nums[index])
+            
+        return [i+1 for i in range(len(nums)) if nums[i]>0]
     
-### better solution
-def moveZeros(nums):
-    z=0 
-    for i in range(len(nums)):
-        if nums[i]!=0:
-            nums[i],nums[z]=nums[z],nums[i]
-            z+=1
+nums=[4,3,2,7,8,2,3,1]
+###Better solution
+def findDisappearedNumbers(nums):
+    return list(set(range(1,len(nums)+1))-set(nums))
